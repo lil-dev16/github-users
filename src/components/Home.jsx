dd86ad4294d60516357d3a628c0f91cccfeb735b
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-21 01:17:51 +0100

    fetch api, styling done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,34 @@
-import React, { useState } from 'react'
-const url = 'api.github.com/users'
+import React, { useEffect, useState } from 'react'
+const url = 'https://api.github.com/users'
 
 const Home = () => {
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
     const getUsers = async() => {
-        await fetch(url)
-        .then(
-            return 
-        )
+        const res = await fetch(url);
+        const data = await res.json();
+        setUsers(data);
+        console.log(data);
     }
+    useEffect(()=> {
+        getUsers();
+    }, [])
   return (
-    <div>Home</div>
+    <div>
+      <h1>GitHub Users</h1>  
+      <div className='cards'>
+        {
+            users.map((user)=> {
+                const {login, avatar_url, id } = user;
+                return (
+                    <div key={id} className= 'card'>
+                        <h3>{login}</h3>
+                        <img src={avatar_url} alt="" width='100px'style={{borderRadius: '50%'}}/>
+                    </div>
+                )
+            })
+        }
+      </div>
+    </div>
   )
 }
 
